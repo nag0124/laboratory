e03917b7ad66ef23f1c545f3fe695c65a9a07d97
nag0124 <wlsdnjs0124@gmail.com>
2024-05-20 17:53:55 +0900

    feat: CreationTimeStamp 적용하여 생성시간 주입하기

--- src/main/java/nag/laboratory/datetime/DateTime.java
+++ src/main/java/nag/laboratory/datetime/DateTime.java
@@ -7,6 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,10 +16,14 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class DateTime extends BaseTimeEntity {
+public class DateTime {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    // 어노테이션 하나면 된다.
+    @CreationTimestamp
+    private Instant createdTime;
 
 }
