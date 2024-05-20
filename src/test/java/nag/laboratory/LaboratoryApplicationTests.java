a0baa5b84848f0982fde875d16eebebf804bd8f5
nag0124 <wlsdnjs0124@gmail.com>
2024-05-20 16:31:55 +0900

    chore: git init

---
+++ src/test/java/nag/laboratory/LaboratoryApplicationTests.java
@@ -0,0 +1,13 @@
+package nag.laboratory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
