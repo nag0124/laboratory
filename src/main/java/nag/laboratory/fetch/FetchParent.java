5ee138bea92680f91641f8e0c67e72ce887be6d4
nag0124 <wlsdnjs0124@gmail.com>
2024-08-15 21:07:55 +0900

    feat: Parent에 OneToMany 옵션 추가

--- src/main/java/nag/laboratory/fetch/FetchParent.java
+++ src/main/java/nag/laboratory/fetch/FetchParent.java
@@ -2,8 +2,10 @@ package nag.laboratory.fetch;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -31,7 +33,7 @@ public class FetchParent {
     @Column()
     private Integer age;
 
-    @OneToMany(mappedBy = "parents")
+    @OneToMany(mappedBy = "parents", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<FetchChild> children;
 
 }
